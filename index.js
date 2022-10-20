d1ff70bf4ca64011d5aa591c36484bc5d2b1c324
oreeeis <110486371+oreeeis@users.noreply.github.com>
2022-10-20 12:20:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ express()
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 600, height: 800 });
+    setTimeout(() => {  console.log("Wait 5secs!"); }, 5000);
     await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
     await page.screenshot({
       path: '/tmp/screenshot.png',
